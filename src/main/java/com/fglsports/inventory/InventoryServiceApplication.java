de52fa2eafaaa14a7efe1526544c3679c58dfe0e
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-07 15:38:05 +0530

    Inventory Service

--- src/main/java/com/fglsports/inventory/InventoryServiceApplication.java
+++ src/main/java/com/fglsports/inventory/InventoryServiceApplication.java
@@ -2,14 +2,25 @@ package com.fglsports.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager("products");
+		return cacheManager;
 	}
 
 }
--- src/main/java/com/fglsports/inventory/Service/ProductService.java
+++ src/main/java/com/fglsports/inventory/Service/ProductService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.fglsports.inventory.Model.Product;
@@ -23,21 +26,24 @@ public class ProductService {
 		productRepository.findAll().forEach(products::add);
 		return products;
 	}
-
+	@Cacheable(value = "products", key = "#id")
 	public Product getProduct(String id) {
 		return productRepository.findOne(id);
 	}
-
+	@CachePut(value = "products", key = "#result.id")
 	public void addProduct(Product product) {
 		productRepository.save(product);
 	}
-
+	@CachePut(value = "products", key = "#product.id")
 	public void updateProduct(String id, Product product) {
 		productRepository.save(product);
 	}
-
+	@CacheEvict(value = "products", key = "#id")
 	public void deleteProduct(String id) {
 		productRepository.delete(id);
+	}
+	@CacheEvict(value = "products", allEntries = true)
+	public void evict() {
 	}
 
 }
